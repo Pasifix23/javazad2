a2160ea456b2de3bd15bc35e6f0ccd693b5c4a70
Pasifix23 <kbezlepkin95@mail.ru>
2022-07-24 13:52:16 +0300

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 10_000;
+
+        int bonus = ticketPrice / 20;
+        System.out.println("Бонусные милли:" + bonus);
+    }
+}
